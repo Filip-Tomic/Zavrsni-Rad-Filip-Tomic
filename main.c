e8a5116d9473a0106d744a14431f6cf9a9dd0fb9
Filip-Tomic <133961083+Filip-Tomic@users.noreply.github.com>
2023-05-18 13:40:02 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include"header.h"
+
+int main() {
+	int izbor;
+	do {
+		ispisIzbornika();
+		scanf("%d", &izbor);
+		switch (izbor) {
+		case 1:
+			ispisiStudente();
+			break;
+		case 2:
+			ispisiStudente();
+			break;
+		case 3:
+			pronadiStudenta();
+			break;
+		case 4:
+			pronadiStudenta();
+			break;
+		case 5:
+			popisSlobodnihSoba();
+			break;
+		case 6:
+			dodajStudenta();
+			break;
+		case 7:
+			uredivanjeStudenata();
+			break;
+		case 8: 
+			brisanjeStudenata();
+			break;
+		default:
+			printf("Neispravan izbor\n");
+			break;
+		}
+
+
+	} while (1);
+
+	return 0;
+}
