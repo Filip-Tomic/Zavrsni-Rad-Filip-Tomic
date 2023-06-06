90eadeff4835a9bfff65a097e40b1127fbf142dd
Filip-Tomic <133961083+Filip-Tomic@users.noreply.github.com>
2023-06-06 13:04:14 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,54 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdlib.h>
+
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include"header.h"
-//Koristenje obveznih koncepata
-//koristenje camelCase
-//10.
-//8.
-//16.
-//6.
-//4.
-
-int main() {
-	int izbor; 
-	do {
-		ispisIzbornika(); //8
-		scanf("%d", &izbor);
-		switch (izbor) {
-		case 1:
-			ispisiStudente();
-			break;
-		case 2:
-			ispisiStudente();
-			break;
-		case 3:
-			pronadiStudenta();
-			break;
-		case 4:
-			pronadiStudenta();
-			break;
-		case 5:
-			popisSlobodnihSoba();
-			break;
-		case 6:
-			dodajStudenta();
-			break;
-		case 7:
-			uredivanjeStudenata();
-			break;
-		case 8: 
-			brisanjeStudenata();
-			break;
-		default:
-			printf("Neispravan izbor\n");
-			break;
-		}
+#include <stdlib.h>
+#include "Header.h"
 
 
-	} while (1);
+int main(void) {
+
+	char* fp = "clanovi.bin"; //16
+	unsigned int STUDENTNumber = 0;
+
+	kreiranje(fp, &STUDENTNumber); //10
+
+	glavniIzbornik(fp, &STUDENTNumber);
 
 	return 0;
 }
